8acb2620d0e1d26b665df9b4fb971a37f8b64216
ClaudioF9 <claudioferreira0284@gmail.com>
2023-01-24 15:30:25 +0000

    Created files, added questions. Timer and question functions now working. Site is able to demonstrate questions and timer counts down. Scoring is also possible

---
+++ assets/js/logic.js
@@ -0,0 +1,144 @@
+//for timer there are two inputs
+
+let buttStart = document.querySelector("#start");
+let timeCount = document.querySelector("#time");
+let screen = document.querySelector(".wrapper");
+
+// console.log(screen);
+// console.log(screen.children[0]);
+
+//variables for the questions-------------------------
+let questionsContainer = document.getElementById("questions");
+let questionTitle = document.getElementById("question-title");
+let choicesContainer = document.getElementById("choices");
+
+//function to add 1st question----------------------------
+// function addQuestions(questions) {
+//     for (let i = 0; i < questions.length; i++) {
+//         questionTitle.innerText = questions[i].title;
+
+//         choicesContainer.innerHTML = "";
+//         for (let q = 0; q < questions[i].choices.length; q++) {
+//             let choice = document.createElement("button");
+//             choice.innerHTML = questions[i].choices[q].answer;
+//             choicesContainer.appendChild(choice);
+//         }
+//     }
+
+let score = 0; // set score to 0
+let currentQuestion = 0;
+
+function addQuestions(questions) {
+    questionTitle.textContent = questions[currentQuestion].title;
+    choicesContainer.textContent = "";
+    for (let q = 0; q < questions[currentQuestion].choices.length; q++) {
+        let choice = document.createElement("button");
+        choice.textContent = questions[currentQuestion].choices[q].answer;
+        choicesContainer.appendChild(choice);
+    }
+}
+
+addQuestions(questionList);
+
+
+// }
+//-----
+// choicesContainer.addEventListener("click", function(event){
+//     if (event.target.tagName === "BUTTON") {
+//         console.log(event.target);
+//     }
+// });
+//---------
+
+
+
+
+// Add a click event listener to the choices container
+choicesContainer.addEventListener("click", function(event){
+    if (event.target.tagName === "BUTTON") {
+        // Check if the clicked button's text matches the correct answer
+        for (let i = 0; i < questionList[currentQuestion].choices.length; i++) {
+            if (event.target.textContent === questionList[currentQuestion].choices[i].answer && questionList[currentQuestion].choices[i].isCorrect) {
+                score++; // Increase the score if the answer is correct
+                break;
+            }
+        }
+        // console.log(`Score: ${score}`);
+        
+        if (currentQuestion < questionList.length - 1) {
+            currentQuestion++;
+            addQuestions(questionList);
+        } else {
+            // console.log("Quiz finished!");
+            // console.log(`Final score: ${score}`);
+            endMessage();
+        }
+    }
+});
+
+
+
+
+
+// choicesContainer.addEventListener("click", function(event){
+// console.log(choice);
+
+// })
+
+// let selection = document.querySelector("choices");
+// choicesContainer.addEventListener("click", function(event){
+//     event.stopPropagation();
+//     console.log(choicesContainer);
+    
+// })
+
+
+
+//Timer Complete Function--------------------------
+let secondsLeft = 60;
+timeCount.textContent = secondsLeft;
+
+//creating the function for the timer
+function countDownTimer() {
+
+    let timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeCount.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+
+            clearInterval(timerInterval);
+            //this will call the endMessage for the highsore
+            endMessage();
+        }
+    }, 1000);
+}
+
+// countDownTimer();
+
+//--------------------------------------------------------
+
+//This is the addeventlistener that the is linked to the button
+buttStart.addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("start-screen").classList.add("hide");
+    document.getElementById("questions").classList.remove("hide");
+    countDownTimer();
+
+    addQuestions(questionList);
+}
+);
+
+//---------------------------------------------------
+
+//function once timer ends
+
+let finalScore = document.getElementById("final-score");
+
+function endMessage(){
+    document.getElementById("questions").classList.add("hide");
+    document.getElementById("end-screen").classList.remove("hide");
+    finalScore.textContent = score;
+};
+
+console.log(questionList);
---
+++ assets/js/questions.js
@@ -0,0 +1,94 @@
+const questionList = [
+    {
+        title: "Which of the following is correct about JavaScript?",
+        choices: [
+            {answer: "JS is an Object-Based Language", isCorrect: true },
+            {answer: "JS is Assembly-language", isCorrect: false},
+            {answer: "JS is an Object-Oriented language", isCorrect: false},
+            {answer: "JS is a High-level language", isCorrect: false}
+        ]
+    },
+    {
+        title: "Arrays in JavaScript are defined by which of the following statements?",
+        choices: [
+            {answer: "It is an ordered list of objects", isCorrect: false },
+            {answer: "It is an ordered list of values", isCorrect: true },
+            {answer: "It is an ordered list of string", isCorrect: false },
+            {answer: "It is an ordered list of functions", isCorrect: false }
+        ] 
+    },
+    {
+        title: "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+        choices: [
+            {answer: "Position", isCorrect: false },
+            {answer: "Window", isCorrect: true },
+            {answer: "Standard", isCorrect: false },
+            {answer: "Location", isCorrect: false }
+        ] 
+    },
+    {
+        title: "Which of the following can be used to call a JavaScript Code Snippet?",
+        choices: [
+            {answer: "Preprocessor", isCorrect: false },
+            {answer: "Triggering Event", isCorrect: false },
+            {answer: "RMI", isCorrect: false },
+            {answer: "Function/Method", isCorrect: true }
+        ] 
+    },
+    {
+        title: "Which of the following methods/operation does javascript use instead of == and !=?",
+        choices: [
+            {answer: "JavaScript uses equalto()", isCorrect: false },
+            {answer: "JavaScript uses equals() and notequals() instead", isCorrect: false },
+            {answer: "JavaScript uses bitwise checking", isCorrect: false },
+            {answer: "JavaScript uses === and !== instead", isCorrect: true }
+        ] 
+    },
+    {
+        title: "Which of the following keywords is used to define a variable in Javascript?",
+        choices: [
+            {answer: "var", isCorrect: false },
+            {answer: "let", isCorrect: false },
+            {answer: "Both A and B", isCorrect: true },
+            {answer: "None of the above", isCorrect: false }
+        ] 
+    },
+    {
+        title: "Which of the following methods is used to access HTML elements using Javascript?",
+        choices: [
+            {answer: "getElementbyId()", isCorrect: false },
+            {answer: "getElementsByClassName()", isCorrect: false },
+            {answer: "Both A and B", isCorrect: true },
+            {answer: "None of the above", isCorrect: false }
+        ] 
+    },
+    {
+        title: "Which of the following methods can be used to display data in some form using Javascript?",
+        choices: [
+            {answer: "document.write()", isCorrect: false },
+            {answer: "console.log()", isCorrect: false },
+            {answer: "window.alert()", isCorrect: false },
+            {answer: "All of the above", isCorrect: true }
+        ] 
+    },
+    {
+        title: "How can a datatype be declared to be a constant type?",
+        choices: [
+            {answer: "var", isCorrect: false },
+            {answer: "const", isCorrect: true },
+            {answer: "let", isCorrect: false },
+            {answer: "constant", isCorrect: false }
+        ] 
+    },
+    {
+        title: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        choices: [
+            {answer: "Boolean", isCorrect: false },
+            {answer: "Undefined", isCorrect: false },
+            {answer: "Object", isCorrect: true },
+            {answer: "Integer", isCorrect: false }
+        ] 
+    },
+]
+
+// console.log(questions);
